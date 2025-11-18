659d276002586b5d9043588ebe2a74fbf0690879
Tanvir <tanvir.info07@gmail.com>
2025-11-19 00:44:37 +0600

    unknown folder delete

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>Hello Home page</div>;
+  return (
+    <>
+      <section class="bg-[url('https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/hero/gridBackground.png')] w-full bg-no-repeat bg-cover bg-center text-sm pb-44 h-screen">
+        <div className="container mx-auto">
+          <h2 className="pt-4">this is app</h2>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default HomePage;
