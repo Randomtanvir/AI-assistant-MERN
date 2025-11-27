355967a32e91f63e61050d93bb3087cffb53c3c3
Tanvir <tanvir.info07@gmail.com>
2025-11-27 14:22:05 +0600

    login and logout function added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,25 +1,10 @@
-import React from "react";
-import { logoutUser } from "../services/authService";
-import useAuth from "../hooks/useAuth";
 import ChatLayout from "../components/homePage/ChatLayout";
 
 const HomePage = () => {
-  const { logout } = useAuth();
-  const handaleLogout = async () => {
-    logout();
-    await logoutUser();
-  };
   return (
     <>
       <section className="min-h-screen bg-linear-to-br from-blue-200 via-sky-300 to-blue-200 backdrop-blur-lg">
         <div className="container mx-auto p-10">
-          <h2 className="pt-4 font-bold">AI Apps---2</h2>
-          <button
-            onClick={handaleLogout}
-            className="bg-red-500 text-white px-3 py-2"
-          >
-            Logout
-          </button>
           <ChatLayout />
         </div>
       </section>
--- frontend/src/pages/SettingPage.jsx
+++ frontend/src/pages/SettingPage.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import AIImageGallery from "../components/settingPage/AIImageGallery";
+import useAuth from "../hooks/useAuth";
+import { logoutUser } from "../services/authService";
 
 const SettingPage = () => {
+  const { logout } = useAuth();
+  const handaleLogout = async () => {
+    logout();
+    await logoutUser();
+  };
   return (
-    <div>
+    <div className="relative">
+      <button
+        onClick={handaleLogout}
+        className="bg-red-500 text-white px-3 py-2 absolute top-5 right-5 hover:bg-red-700 rounded-sm cursor-pointer"
+      >
+        Logout
+      </button>
       <AIImageGallery />
     </div>
   );
